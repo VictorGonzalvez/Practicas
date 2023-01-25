64cc57b3e5076d7e7e1f8c3ccfdc1df9b7dcbd32
Victor Gonzalvez <victorduski@gmail.com>
2023-01-25 18:00:30 -0300

    ejercicio1.3

--- miduBootcamp/part1/src/index.js
+++ miduBootcamp/part1/src/index.js
@@ -1,15 +1,11 @@
 import React, { version } from 'react'
 import ReactDOM from 'react-dom'
 
-/*
-const Title = ({course}) => <h1>{course}</h1>
-*/
 
-const Title = (props) => {
-  return <h1>{props.course}</h1>
-}
+const Title = ({course}) => <h1>{course}</h1>
 const Parrafo= ({frase,numero} )=> <p>{frase}  {numero}</p>
 const Calcular = ({a,b,c}) => <p>Number of excercises {a+b+c}</p>
+/*Parte React
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -18,7 +14,6 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  
   return (
     <div>
       <Title course = {course}/>
@@ -29,6 +24,32 @@ const App = () => {
         <Calcular a={exercises1} b={exercises2} c={exercises3} />
     </div>
   )
+}
+*/
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 ={
+    name: 'Fundamentals of React',
+    excercises: 10
+  }
+  const part2 ={
+    name: 'Using props to pass data',
+    excercises: 7
+  }
+  const part3 ={
+    name: 'State of a component',
+    excercises: 14
+  }
+  return(
+    <div>
+      <Title course={course}/>
+      <Parrafo frase={part1.name} numero={part1.excercises}/>
+      <Parrafo frase={part2.name} numero={part2.excercises}/>
+      <Parrafo frase={part3.name} numero={part3.excercises}/>
+      <Calcular a={part1.excercises} b={part2.excercises} c={part3.excercises}/>
+    </div>
+  )
+
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
